da92e524ee02d4b2f230c2e7f43a262341efe386
Pratyaksh Gupta ᵍᵐ <onchainengineer@proton.me>
2025-09-21 20:25:36 +0530

    Create rlhub-cli.rb

---
+++ rlhub-cli.rb
@@ -0,0 +1,33 @@
+class RlhubCli < Formula
+  desc "Enterprise-grade RL training infrastructure for everyone"
+  homepage "https://github.com/rlhub-dev/cli"
+  version "2.2.0"  # Auto-updated by GitHub Actions
+  license "MIT"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/rlhub-dev/cli/releases/download/v#{version}/rlhub-cli-v#{version}-macos-arm64"
+    sha256 "sha256:4e832ee07535c3ef35db5af5a6bd2a824b330270d0fb98796171fe8b59e624a8"  # Auto-updated by GitHub Actions
+  else
+    url "https://github.com/rlhub-dev/cli/releases/download/v#{version}/wirtual-v#{version}-macos-amd64"
+    sha256 "PLACEHOLDER_AMD64_SHA"  # Auto-updated by GitHub Actions
+  end
+
+  def install
+    bin.install "rlhub-cli-v#{version}-macos-#{Hardware::CPU.arch}" => "wirtual"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/wirtual --version")
+  end
+
+  def caveats
+    <<~EOS
+      wirtual has been installed successfully!
+      
+      Get started:
+        $ wirtual --help
+      
+      Documentation: https://rlhub.dev/docs
+    EOS
+  end
+end
